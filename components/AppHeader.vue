837e94ce51f47695dd13eab9a938ce397f068d5c
Саша <115547303+1Musl1m@users.noreply.github.com>
2024-06-07 23:22:14 +0300

    add tailwind and nav

---
+++ components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full">
+        <nav class="flex justify-between items-center">
+            <img src="/public/navbar/Group 29.svg"/>
+            <div>
+                <ul class="flex items-center gap-3">
+                    <li><img src="/public/navbar/search.svg" width="37px"/></li>
+                    <li><img src="/public/navbar/user.svg" width="37px"/></li>
+                    <li><img src="/public/navbar/add.svg" width="37px"/></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
