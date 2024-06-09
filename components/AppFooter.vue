440130a9cb2acb8cdaa0cc394fac4801f275b1c7
Саша <115547303+1Musl1m@users.noreply.github.com>
2024-06-09 15:57:43 +0300

    add footer

---
+++ components/AppFooter.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class="w-full bg-gray-200 p-4 rounded flex items-center justify-between">
+        <img src="/public/navbar/Group 29.svg"/>
+        <div>
+            <ul class="flex items-center gap-3">
+                <li><NuxtLink to="/">Developers</NuxtLink></li>
+                <li><NuxtLink to="/">Pricing</NuxtLink></li>
+                <li><NuxtLink to="/">Support</NuxtLink></li>
+            </ul>
+        </div>
+    </footer>
+</template>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -4,9 +4,9 @@
             <img src="/public/navbar/Group 29.svg"/>
             <div>
                 <ul class="flex items-center gap-3">
-                    <li><img src="/public/navbar/search.svg" width="37px"/></li>
-                    <li><img src="/public/navbar/user.svg" width="37px"/></li>
-                    <li><img src="/public/navbar/add.svg" width="37px"/></li>
+                    <li><NuxtLink to="/"><img src="/public/navbar/search.svg" width="37px"/></NuxtLink></li>
+                    <li><NuxtLink to="/"><img src="/public/navbar/user.svg" width="37px"/></NuxtLink></li>
+                    <li><NuxtLink to="/"><img src="/public/navbar/add.svg" width="37px"/></NuxtLink></li>
                 </ul>
             </div>
         </nav>
